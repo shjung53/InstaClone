c7695c90580b7523702fbdf397311f91b377b16e
shjung53 <gcjung777@gmail.com>
2022-02-24 23:31:58 +0900

    search rv making..

--- app/src/main/java/com/example/instagramClone/SearchFragment.kt
+++ app/src/main/java/com/example/instagramClone/SearchFragment.kt
@@ -1,6 +1,19 @@
 package com.example.instagramClone
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.instagramClone.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment() {
+    lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
