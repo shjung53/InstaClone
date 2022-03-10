32388bc0b4f3f8939e50dc1186511845d832bbfc
shjung53 <gcjung777@gmail.com>
2022-03-10 23:35:05 +0900

    grid

--- app/src/main/java/com/example/instagramClone/SearchFragment.kt
+++ app/src/main/java/com/example/instagramClone/SearchFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.instagramClone.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment() {
@@ -13,6 +14,7 @@ class SearchFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+
 
         return binding.root
     }
