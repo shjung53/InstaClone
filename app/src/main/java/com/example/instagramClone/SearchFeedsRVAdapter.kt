d75b37595a70bbe3bcc034c2a62dda4fe841d3e8
shjung <gcjung777@gmail.com>
2022-03-12 20:56:49 +0900

    grid layout

--- app/src/main/java/com/example/instagramClone/SearchFeedsRVAdapter.kt
+++ app/src/main/java/com/example/instagramClone/SearchFeedsRVAdapter.kt
@@ -3,24 +3,25 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagramClone.databinding.ItemFeedBinding
+import com.example.instagramClone.databinding.ItemSearchFeedBinding
 
-class SearchFeedsRVAdapter(): RecyclerView.Adapter<SearchFeedsRVAdapter.ViewHolder>() {
+class SearchFeedsRVAdapter(var feedsItem: ArrayList<FeedData>): RecyclerView.Adapter<SearchFeedsRVAdapter.ViewHolder>() {
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int
     ): SearchFeedsRVAdapter.ViewHolder {
-        val binding: ItemFeedBinding = ItemFeedBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        val binding: ItemSearchFeedBinding = ItemSearchFeedBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
 
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: SearchFeedsRVAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = feedsItem.size
 
-    inner class ViewHolder(binding: ItemFeedBinding): RecyclerView.ViewHolder(binding.root){
-
+    inner class ViewHolder(private val binding: ItemSearchFeedBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(feedData: FeedData){
+            binding.itemSearchFeedIv.setImageResource(feedData.feedImg)
+        }
     }
 }
